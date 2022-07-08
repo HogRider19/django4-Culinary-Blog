ce6ff9f137dbae3687ba13ac21e0fc18d2ecc801
HogRider <98631205+HogRider19@users.noreply.github.com>
2022-07-08 23:28:10 +0300

    Finished blog models!

--- CulinaryBlog/blog/models.py
+++ CulinaryBlog/blog/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Category(MPTTModel):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100)
-    parent = models.TreeForeignKey(
+    parent = TreeForeignKey(
         'self',
         related_name='children',
         on_delete=models.SET_NULL,
@@ -36,3 +36,31 @@ class Post(models.Model):
     )
     tags = models.ManyToManyField(Tag, related_name='past')
     create_at = models.DateTimeField(auto_now_add=True)
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+    serves = models.CharField(max_length=50)
+    prep_time = models.PositiveIntegerField(blank=True, null=True)
+    cook_time = models.PositiveIntegerField(blank=True, null=True)
+    ingredients = models.TextField()
+    directions = models.TextField()
+    post = models.ForeignKey(
+        Post,
+        related_name='recipe',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+
+class Comment(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    website = models.CharField(max_length=200)
+    message = models.TextField()
+    post = models.ForeignKey(
+        Post,
+        related_name = 'comment',
+        on_delete=models.CASCADE
+    )
