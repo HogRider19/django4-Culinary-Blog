571163ea37972516bda2c04e160a921a16715b1f
HogRider <98631205+HogRider19@users.noreply.github.com>
2022-07-15 02:19:27 +0300

    Finished sidebar!

--- CulinaryBlog/blog/templatetags/menu_tags.py
+++ CulinaryBlog/blog/templatetags/menu_tags.py
@@ -5,9 +5,20 @@ from blog.models import Category, Post
 register = template.Library()
 
 
+def get_all_categories():
+    """Хешируемая фекция со всеми категориями"""
+    return Category.objects.all()
+
+
+@register.simple_tag()
+def get_list_categories():
+    """Вывод всех категорий"""
+    return get_all_categories()
+
+
 @register.inclusion_tag('include/tags/top_menu.html')
 def get_categories():
-    category = Category.objects.all()
+    category = get_all_categories()
     return {"list_category": category}
     
 
